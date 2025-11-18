277a881e6fdbe2672d6815fef31060ba43edec32
Nishu Ranjit <nishuranjit55@gmail.com>
2025-11-18 22:04:51 +0545

    project completed

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 // Stripe API configuration
 $stripe_keys = [
-    'secret' => 'sk_test_51QtsOJFA89whLkDzGm4kN1OAzgmhGkyvq5QhKpYkg6v0uuDffojUBl6ai4whoQ6ehEp1y1iKB8uAgvfpzfyM75ql00rEqqX93S',
-    'publishable' => 'pk_test_51QtsOJFA89whLkDzl2wQYtWNVymWFcOAlQ9MVYM8y2fob5tN14mwxQBHKrZkzlY8BDGP0mdGBQjS4mk9p9MZT97F00G3GyV6ra'
+    'secret' => '',
+    'publishable' => ''
 ];
 
 // Load Stripe SDK (if not already included)
